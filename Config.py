1c5c6d4a135c81e996e4146cfda7b0ac16419df3
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-11-18 22:02:07 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,12 +4,12 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
+        API_ID = int(os.environ.get('API_ID', 21518327))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    API_HASH = os.environ.get('API_HASH', e72f588b3e4763f01eecfc3c4aa7e8ac)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', 6721031289:AAF2orQmnkkSCJC7Rp7-fSehRaezexSRNtE)
+    DATABASE_URL = os.environ.get('DATABASE_URL', postgres://irnfieon:K-F9J_rI1BgfI9tsoVU_jZ1NvmfABWLF@berry.db.elephantsql.com/irnfieon)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
